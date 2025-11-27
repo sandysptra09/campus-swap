aa40659bb64cc11dba59dcd5f4e05c78d5d31cec
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-11-27 19:12:07 +0700

    style(section): adjusting padding in quick actions section

--- src/components/customs/section/main/quick-actions.tsx
+++ src/components/customs/section/main/quick-actions.tsx
@@ -6,7 +6,7 @@ import QuickActionCard from '../../cards/quick-action-card';
 
 export default function QuickActionsSection() {
     return (
-        <section className='max-w-6xl mx-auto px-6 md:px-12 lg:px-20 py-10'>
+        <section className='max-w-6xl mx-auto px-6 md:px-12 lg:px-20 py-6'>
             <QuickActionCard />
         </section>
     )
