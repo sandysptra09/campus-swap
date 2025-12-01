35d44d8d25bde1a9acf997f854bc3ada04270440
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-12-01 18:02:40 +0700

    refactor(support): changes file name privacy policy in pages

--- src/app/(user)/support/privacy-policy/page.tsx
+++ src/app/(user)/support/privacy-policy/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 
-import PrivacyPolicyPage from '@/pages/(support)/privacy-policy/privacy-policy';
+import PrivacyPolicyPage from '@/pages/(support)/privacy-policy/privacy-policy-page';
 
 export default function PrivacyAndPolicyPage() {
     return (
