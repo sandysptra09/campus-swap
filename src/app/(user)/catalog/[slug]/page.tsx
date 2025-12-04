ab72076f9410624a68137751473925a1a694b7e2
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-12-04 19:20:38 +0700

    feat: add a product desctiption section

--- src/app/(user)/catalog/[slug]/page.tsx
+++ src/app/(user)/catalog/[slug]/page.tsx
@@ -3,11 +3,13 @@
 import React from 'react'
 
 import ProductInformationSection from '@/components/customs/section/catalog/catalog-detail/product-information';
+import ProductDescriptionSection from '@/components/customs/section/catalog/catalog-detail/product-description';
 
 export default function CatalogDetailPage() {
     return (
         <main className='w-full min-h-screen'>
             <ProductInformationSection />
+            <ProductDescriptionSection />
         </main>
     )
 }
---
+++ src/components/customs/section/catalog/catalog-detail/product-description.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react'
+
+export default function ProductDescriptionSection() {
+    return (
+        <section className='max-w-6xl mx-auto px-6 md:px-10 lg:px-20 py-2 md:py-4'>
+            <h2 className='text-xl md:text-2xl font-semibold mb-2 text-foreground'>
+                Product Description
+            </h2>
+            <p className='whitespace-pre-line text-muted-foreground leading-relaxed text-sm md:text-base'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus vero ullam alias itaque tempora, repellendus odit fugit dolor, cumque quasi corrupti incidunt veritatis nobis modi commodi, corporis accusantium magni. Necessitatibus!
+                Similique quos reiciendis ipsam, aliquid corrupti sed ratione quam in asperiores delectus qui illum placeat non, optio natus modi quidem distinctio? Aut obcaecati labore dolores sed delectus exercitationem impedit quisquam.
+                Vitae eius tempore vel nam exercitationem molestiae nemo libero, dicta nisi non at expedita modi dignissimos minima quam pariatur blanditiis est veritatis suscipit repellat ipsa rem commodi, earum cumque. Officia.
+                Illum deleniti ratione ipsam sapiente distinctio? Ipsam deleniti eligendi sequi neque a, dolorem consequuntur deserunt. Itaque deleniti quasi provident tempora odio exercitationem consectetur repellat ipsum id, alias facere dignissimos! Officia?
+                Minus, nemo harum? Nam deserunt, veritatis ipsam, unde in cumque aspernatur fuga nemo laborum quia officia suscipit vero enim? Perspiciatis, maiores alias facere doloremque nam iure et consectetur eum quam!
+            </p>
+        </section>
+    )
+}
