eaaa627f0e50ea857f8cdf25dc2938cf8dc7328c
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-12-14 20:10:41 +0700

    feat: add a initial admin dashboard page

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react'
+
+export default function AdminDashboardPage() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam deleniti blanditiis tempore veniam molestias alias quis dolore nemo, totam minus corporis, accusantium libero officiis adipisci magnam hic at sint rerum?
+            Voluptatum, eaque assumenda eos distinctio consequuntur quibusdam animi beatae obcaecati cum iusto in laboriosam facere illum, excepturi modi ipsam repudiandae esse asperiores tempore sequi nemo voluptatibus repellat blanditiis? Quos, velit.
+            Maxime ullam unde illum rerum sed quisquam, natus repudiandae itaque nihil inventore adipisci sit consequatur. Commodi cupiditate, iure numquam ratione iusto corporis, deleniti sequi incidunt inventore consectetur assumenda voluptatem impedit.
+            Fuga quos explicabo numquam suscipit inventore, voluptatum totam alias, rem sit tempore quam temporibus necessitatibus ab repellendus a doloremque magni! Quia exercitationem saepe possimus, neque esse odio minus harum eaque.
+            Voluptate eveniet fugiat dolorem iusto, dolorum iure quas. Necessitatibus voluptatem cum aperiam enim, vitae veritatis sunt, aliquid porro velit at obcaecati officia placeat. Eaque molestiae doloribus illo dolorem, quidem adipisci.
+        </div>
+    )
+}
